3a2571657acce53c7d38b1ebfbfae99e7cf7100a
web-lake <vladimirozerskiy@gmail.com>
2018-09-16 21:58:43 +0300

    Create a submission component, integrate it to markup

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+const submissionComponent = {
+  template:
+    `  <div style="display: flex; width: 100%">
+      <figure class="media-left">
+        <div class="image is-64x64">
+          <img v-bind:src="submission.submissionImage">
+        </div>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>{{ submission.title }}</strong> <small><a v-bind:href="submission.url">{{ submission.url }}</a></small> <small class="tag is-primary">{{ submission.tag }}</small>
+            <br>
+            {{ submission.description }}
+          </p>
+          <p>Submitted by: <img class="image is-24x24" v-bind:src="submission.avatar" alt="user photo">
+            {{ submission.submitter }}</p>
+        </div>
+      </div>
+      <div class="media-right has-text-centered">
+        <button @click="upvote(submission.id)" class="button is-link is-outlined">
+          <span class="icon">
+            <i class="far fa-thumbs-up"></i>
+          </span>
+        </button>
+        <p class="has-text-link has-text-centered">{{ submission.votes }}</p>
+      </div>
+    </div>`,
+  props: ['submission', 'submissions'],
+  methods: {
+    upvote(submissionId) {
+      const submission = this.submissions.find(submission => submission.id === submissionId);
+      submission.votes++;
+    }
+  }
+};
+
+
 new Vue({
   el: '#app',
   data: {
@@ -10,10 +48,7 @@ new Vue({
       });
     }
   },
-  methods: {
-    upvote(submissionId) {
-      const submission = this.submissions.find(submission => submission.id === submissionId);
-      submission.votes++;
-    }
+  components: {
+    'submission-component': submissionComponent
   }
 });
